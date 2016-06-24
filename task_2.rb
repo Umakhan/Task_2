bf076efbcfdfdccd46a65684ebf2b963bcd91506
Saypudinov U <abrakad55@gmail.com>
2016-06-24 16:19:00 +0300

    Edit classes

--- task_2.rb
+++ task_2.rb
@@ -3,18 +3,18 @@ require 'mechanize'
 
 class ExchangeUSD
 
-	attr_reader   :dollar, :path, :page, :review_meta
+	attr_reader   :dollar, :path
 	attr_writer   :dollar
-
+	
 	def initialize
 		@a = Mechanize.new
 		@path = 'div.switcher_view.cbr_view table.table.horizontal tbody tr td'
 	end
 
 	def parser
-		@page = @a.get('http://www.raiffeisen.ru/ru/currency_rates/')
-		@review_meta = @page.search(@path) 
-		@dollar.to_f*@review_meta[3].text.to_f
+		page = @a.get('http://www.raiffeisen.ru/ru/currency_rates/')
+		review_meta = page.search(@path)
+		@dollar.to_f*review_meta[3].text.to_f
 	end
 end		
 
